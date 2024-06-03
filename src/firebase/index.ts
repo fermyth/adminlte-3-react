7b86e2eb2136901ff38f1ef11eae8f6e7daad09e
alsyah07 <46066495+alsyah07@users.noreply.github.com>
2024-06-03 16:19:43 +0700

    update

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,15 +1,21 @@
 import { FirebaseOptions, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-
-const { VITE_FIREBASE_CONFIG } = import.meta.env;
-
-if (!VITE_FIREBASE_CONFIG) {
-  throw new Error('Firebase config is missing!');
-}
-
-const firebaseConfig: FirebaseOptions = JSON.parse(VITE_FIREBASE_CONFIG);
-
+ 
+// const { VITE_FIREBASE_CONFIG } = import.meta.env;
+ 
+// if (!VITE_FIREBASE_CONFIG) {
+//   throw new Error('Firebase config is missing!');
+// }
+ 
+const firebaseConfig={
+  apiKey: "AIzaSyBWRJMJaYe1ktgqAXn8IMTF02FiAa6nqvg",
+  authDomain: "backendtest-af756.firebaseapp.com",
+  projectId: "backendtest-af756",
+  storageBucket: "backendtest-af756.appspot.com",
+  messagingSenderId: "896050025006",
+  appId: "1:896050025006:web:8f8051c87841c32f5a05bf"};
+ 
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-
+ 
 export const firebaseAuth = getAuth(app);
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,75 @@
-import { InfoBox } from '@app/components/info-box/InfoBox';
-import { ContentHeader } from '@components';
+import React from "react";
+import { InfoBox } from "@app/components/info-box/InfoBox";
+import { ContentHeader } from "@components";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar, Pie } from "react-chartjs-2";
+
+// Registrasi komponen yang diperlukan
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 
 const Dashboard = () => {
+  const data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)",
+        borderWidth: 1,
+        hoverBackgroundColor: "rgba(75,192,192,0.4)",
+        hoverBorderColor: "rgba(75,192,192,1)",
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+    ],
+  };
+
+  const datapie = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Monthly Sales",
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 99, 132, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+          "rgba(255, 99, 132, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
     <div>
       <ContentHeader title="Dashboard" />
@@ -13,7 +81,6 @@ const Dashboard = () => {
               <div className="small-box bg-info">
                 <div className="inner">
                   <h3>150</h3>
-
                   <p>New Orders</p>
                 </div>
                 <div className="icon">
@@ -28,9 +95,8 @@ const Dashboard = () => {
               <div className="small-box bg-success">
                 <div className="inner">
                   <h3>
-                    53<sup style={{ fontSize: '20px' }}>%</sup>
+                    53<sup style={{ fontSize: "20px" }}>%</sup>
                   </h3>
-
                   <p>Bounce Rate</p>
                 </div>
                 <div className="icon">
@@ -45,7 +111,6 @@ const Dashboard = () => {
               <div className="small-box bg-warning">
                 <div className="inner">
                   <h3>44</h3>
-
                   <p>User Registrations</p>
                 </div>
                 <div className="icon">
@@ -60,7 +125,6 @@ const Dashboard = () => {
               <div className="small-box bg-danger">
                 <div className="inner">
                   <h3>65</h3>
-
                   <p>Unique Visitors</p>
                 </div>
                 <div className="icon">
@@ -74,7 +138,37 @@ const Dashboard = () => {
           </div>
           <div className="row">
             <div className="col-lg-3 col-6">
-              <InfoBox variant='warning' title="Messages" text="1,410" />
+              <InfoBox variant="warning" title="Messages" text="1,410" />
+            </div>
+            <div className="col-lg-9 col-12">
+              <div className="card">
+                <div className="card-header">
+                  <h3 className="card-title">Sales Data - Bar Chart</h3>
+                </div>
+                <div className="card-body">
+                  <Bar
+                    data={data}
+                    options={{
+                      maintainAspectRatio: false,
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-12">
+              <div className="card">
+                <div className="card-header">
+                  <h3 className="card-title">Sales Data - Pie Chart</h3>
+                </div>
+                <div className="card-body">
+                  <Pie
+                    data={datapie}
+                    options={{
+                      maintainAspectRatio: false,
+                    }}
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -84,3 +178,4 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
