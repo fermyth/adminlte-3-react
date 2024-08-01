644738179cfae784f4b5542fe2d404da2974774c
Arbarainn <muhaaliakbar2312@gmail.com>
2024-08-01 13:43:32 +0700

    update Api

--- src/libs/Api.ts
+++ src/libs/Api.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 
 const ApiConfig = axios.create({
   // baseURL: "http://localhost:5180/api/v1",
- baseURL : "https://api_portal.sigapdriver.com/api/v1/"
+ baseURL : "https://api_portal.sigapdriver.com/api/v1"
 //baseURL : "https://backend.sigapdriver.com/api/"
 });
 
 const UrlServer = () => {
-  return "https://backend.sigapdriver.com/";
+  return "https://api_portal.sigapdriver.com";
 }
 
 export { UrlServer };
