5c9d4605bd73a13851522af197fbac6c199066d7
Arkan <146117426+arkan0303@users.noreply.github.com>
2024-08-13 13:58:47 +0700

    footer update

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,21 +1,26 @@
 const Footer = () => {
     return (
     <>
-            <style>
-    {`
-    .version-info {
-          font-size: 0.875rem;
-          color: #6c757d;
-          margin-top: 1rem;
-          text-align: center;
+        <style>
+        {`
+        .version-info {
+            font-size: 0.875rem;
+            color: #6c757d;
+            margin-top: 1rem;
+            text-align: center;
+        }
+   
+        .footer-container {
+            width: 75%;
+            margin: 0 auto;
         }
         `}
-    </style>
-    <div className=" w-75 ">
-      <p className="version-info">Version 1.2</p>
-      </div>
+        </style>
+        <div className="footer-container">
+            <p className="version-info">Version 1.2</p>
+        </div>
     </>
     )
-}
-
-export default Footer
+  }
+   
+  export default Footer;
