8a7b4faebd7e97b06fce35e55ce3bd9a5d00503b
alsyah07 <46066495+alsyah07@users.noreply.github.com>
2024-08-13 19:51:58 +0700

    Update Api.ts

--- src/libs/Api.ts
+++ src/libs/Api.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const ApiConfig = axios.create({
   // baseURL: "http://localhost:5181/api/v1",
- //baseURL : "https://api_portal.sigapdriver.com/api/v1"
- baseURL:"https://api_portal_staging.sigapdriver.com/api/v1";
-//baseURL : "https://backend.sigapdriver.com/api/" 
+  // baseURL: "https://api_portal.sigapdriver.com/api/v1",
+  baseURL: "https://api_portal_staging.sigapdriver.com/api/v1",
+  // baseURL: "https://backend.sigapdriver.com/api/"
 });
 
 
