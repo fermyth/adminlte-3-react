33248338109b653fd9b878b42755550c74763745
Arkan <146117426+arkan0303@users.noreply.github.com>
2024-08-15 00:20:34 +0700

    update dashboard

--- src/pages/partner/Dashboard/Dashboard_Partner.tsx
+++ src/pages/partner/Dashboard/Dashboard_Partner.tsx
@@ -5,9 +5,12 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useLocation } from "react-router-dom";
 
 const DashboardPartner = () => {
-  const [companies , setCompanies] = useState([]);
-  const [mobilContarct , setmobilContarct] = useState([]);
-  const [mobilContarct2 , setmobilContarct2] = useState([]);
+  const [companies , setCompanies] = useState<any>([]);
+  const [mobilContarct , setmobilContarct] = useState<any>([]);
+  const [mobilContarct2 , setmobilContarct2] = useState<any>([]);
+  const [stnk , setStnk] = useState<any>([]);
+  const [emisi , setEmisi] = useState<any>([]);
+  const [service , setService] = useState<any>([]);
   const [idCompany, setIdCompany] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
@@ -83,6 +86,54 @@ const DashboardPartner = () => {
     fetchData();
   }
   }, [idCompany, location.key]);
+  useEffect(() => {
+    if(idCompany){
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:5182/api/v1/jadwals-stnk-renewal/${idCompany}`);
+        const data = await response.json();
+        console.log("stnk", data);
+        
+        setStnk(data);
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+      }
+    };
+    fetchData();
+  }
+  }, [idCompany, location.key]);
+  useEffect(() => {
+    if(idCompany){
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:5182/api/v1/jadwals-emission-test/${idCompany}`);
+        const data = await response.json();
+        console.log("asasas", data);
+        
+        setEmisi(data);
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+      }
+    };
+    fetchData();
+  }
+  }, [idCompany, location.key]);
+  useEffect(() => {
+    if(idCompany){
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:5182/api/v1/jadwals-service_rutin/${idCompany}`);
+        const data = await response.json();
+        console.log("asasas", data);
+        
+        setService(data);
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+      }
+    };
+    fetchData();
+  }
+  }, [idCompany, location.key]);
 
   return (
     <>
@@ -108,7 +159,9 @@ const DashboardPartner = () => {
               <h5 className="card-title text-center font-weight-bold">
                 No. Of Customer
               </h5>
-              <h1 className="card-text text-center font-weight-bold">{companies ? companies.totalPerusahaan : "-" }</h1>
+              <h1 className="card-text text-center font-weight-bold">  
+      {(companies && companies.totalPerusahaan > 0) ? companies.totalPerusahaan : "-"}  
+    </h1> 
             </div>
           </Col>
           <Col>
@@ -116,7 +169,9 @@ const DashboardPartner = () => {
               <h5 className="card-title text-center font-weight-bold">
                 No. Of Vehicle
               </h5>
-              <h1 className="card-text text-center font-weight-bold">{companies ? companies.totalMobil : "-" }</h1>
+              <h1 className="card-text text-center font-weight-bold">  
+      {(companies && companies.totalMobil > 0) ? companies.totalMobil : "-"}  
+    </h1> 
             </div>
           </Col>
           <Col>
@@ -124,59 +179,118 @@ const DashboardPartner = () => {
               <h5 className="card-title text-center font-weight-bold">
               expiring Contract
               </h5>
-              <h1 className="card-text text-center font-weight-bold">{mobilContarct ? mobilContarct.totalMobilEndingSoon : "-"}</h1>
+              <h1 className="card-text text-center font-weight-bold">  
+      {(mobilContarct && mobilContarct.totalMobilEndingSoon > 0) ? mobilContarct.totalMobilEndingSoon : "-"}  
+    </h1> 
             </div>
           </Col>
         </Row>
       </Container>
       <div className="container mt-5">
-        <div className="card">
-          <div className="card-header text-dark">Updates</div>
-          <div className="card-body">
-            <div className="row">
-              <div className="col-md-12">
-                <div
-                  className=""
-                  style={{
-                    backgroundColor: "#009879",
-                    color: "white",
-                    padding: "10px",
-                    width: "100%",
-                    margin: "10px",
-                    transition: "transform 0.3s ease-in-out",
-                  }}
-                >
-                  <i className="fa fa-life-ring" aria-hidden="true"></i> {mobilContarct2 ? mobilContarct2.totalMobilEndingSoon : "-"}
-                 {" "} vehicle's contracts are due in 60 Days{" "}
-                  <a href="#" className="alert-link">
-                    details
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-12">
-                <div
-                  className=""
-                  style={{
-                    backgroundColor: "#009879",
-                    color: "white",
-                    padding: "10px",
-                    width: "100%",
-                    margin: "10px",
-                    transition: "transform 0.3s ease-in-out",
-                  }}
-                >
-                  <i className="fa fa-life-ring" aria-hidden="true"></i> 5
-                  vehicles are scheduled in maintenance in 20 days{" "}
-                  <a href="#" className="alert-link">
-                    details
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="card">  
+  <div className="card-header text-dark">Updates</div>  
+  <div className="card-body">  
+    {(mobilContarct2?.totalMobilEndingSoon === 0 &&  
+      stnk?.totalCount === 0 &&  
+      emisi?.totalCount === 0 &&  
+      service?.totalCount === 0) ? (  
+      <div style={{ color: "black", padding: "", margin: "" }}>  
+        Data belum tersedia  
+      </div>  
+    ) : (  
+      <>  
+        {mobilContarct2?.totalMobilEndingSoon > 0 && (  
+          <div className="row">  
+            <div className="col-md-12">  
+              <div  
+                style={{  
+                  backgroundColor: "#009879",  
+                  color: "white",  
+                  padding: "10px",  
+                  width: "100%",  
+                  margin: "10px",  
+                  transition: "transform 0.3s ease-in-out",  
+                }}  
+              >  
+                <i className="fa fa-life-ring" aria-hidden="true"></i> {mobilContarct2.totalMobilEndingSoon}{" "}  
+                vehicle's contracts are due in 60 Days{" "}  
+                <a href="#" className="alert-link">  
+                  details  
+                </a>  
+              </div>  
+            </div>  
+          </div>  
+        )}  
+        {stnk?.totalCount > 0 && (  
+          <div className="row">  
+            <div className="col-md-12">  
+              <div  
+                style={{  
+                  backgroundColor: "#009879",  
+                  color: "white",  
+                  padding: "10px",  
+                  width: "100%",  
+                  margin: "10px",  
+                  transition: "transform 0.3s ease-in-out",  
+                }}  
+              >  
+                <i className="fa fa-life-ring" aria-hidden="true"></i> {stnk.totalCount}{" "}  
+                vehicles need to renew their STNK in 30 days{" "}  
+                <a href="#" className="alert-link">  
+                  details  
+                </a>  
+              </div>  
+            </div>  
+          </div>  
+        )}  
+        {emisi?.totalCount > 0 && (  
+          <div className="row">  
+            <div className="col-md-12">  
+              <div  
+                style={{  
+                  backgroundColor: "#009879",  
+                  color: "white",  
+                  padding: "10px",  
+                  width: "100%",  
+                  margin: "10px",  
+                  transition: "transform 0.3s ease-in-out",  
+                }}  
+              >  
+                <i className="fa fa-life-ring" aria-hidden="true"></i> {emisi.totalCount}{" "}  
+                vehicles scheduled to have emission test in 30 days{" "}  
+                <a href="#" className="alert-link">  
+                  details  
+                </a>  
+              </div>  
+            </div>  
+          </div>  
+        )}  
+        {service?.totalCount > 0 && (  
+          <div className="row">  
+            <div className="col-md-12">  
+              <div  
+                style={{  
+                  backgroundColor: "#009879",  
+                  color: "white",  
+                  padding: "10px",  
+                  width: "100%",  
+                  margin: "10px",  
+                  transition: "transform 0.3s ease-in-out",  
+                }}  
+              >  
+                <i className="fa fa-life-ring" aria-hidden="true"></i> {service.totalCount}{" "}  
+                vehicles scheduled to have service routine test in 30 days{" "}  
+                <a href="#" className="alert-link">  
+                  details  
+                </a>  
+              </div>  
+            </div>  
+          </div>  
+        )}  
+      </>  
+    )}  
+  </div>  
+</div>
       </div>
       <div>
         <CustomerList />
