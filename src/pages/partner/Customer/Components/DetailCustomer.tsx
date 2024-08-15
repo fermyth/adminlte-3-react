b22a9e94c9c44886bc2eadfae92795444f7e5620
alsyah07 <46066495+alsyah07@users.noreply.github.com>
2024-08-15 12:27:34 +0700

    Update DetailCustomer.tsx

--- src/pages/partner/Customer/Components/DetailCustomer.tsx
+++ src/pages/partner/Customer/Components/DetailCustomer.tsx
@@ -99,14 +99,14 @@ const CustomerDetail: React.FC = () => {
         setIsLoading(true);
         try {
           const driversResponse = await ApiConfig.get(
-            // `https://api_partner_staging.sigapdriver.com/api/v1/mobil/${idperusahaan}`
-             `http://localhost:5182/api/v1/mobil/${idperusahaan}`
+             `https://api_partner_staging.sigapdriver.com/api/v1/mobil/${idperusahaan}`
+            // `http://localhost:5182/api/v1/mobil/${idperusahaan}`
           );
           console.log("Drivers response:", driversResponse.data);
 
           const fullNamesResponse = await ApiConfig.get(
-            // `https://api_portal_staging.sigapdriver.com/api/v1/nopoldriver/`
-            `http://localhost:5181/api/v1/nopoldriver/`
+             `https://api_portal_staging.sigapdriver.com/api/v1/nopoldriver/`
+           // `http://localhost:5181/api/v1/nopoldriver/`
           );
           const fullNamesData = fullNamesResponse.data.data.data;
           console.log("Drivers responsedd:", fullNamesResponse);
