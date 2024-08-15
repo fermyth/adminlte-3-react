82d1ac33229c3f15c7d95937c6b677590592c696
Arkan <146117426+arkan0303@users.noreply.github.com>
2024-08-15 13:25:34 +0700

    update vehicle

--- src/pages/partner/Vehicle/Vehicle.tsx
+++ src/pages/partner/Vehicle/Vehicle.tsx
@@ -1,24 +1,73 @@
-import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit';
-
-
+import { useLocation } from 'react-router-dom';
 
 const Vehicle = () => {
-  // Data sampel untuk tabel
-  const vehicles = [
-    { id: 1, policeNumber: 'B 1234 XYZ', unitType: 'SUV', contractEnd: '2024-12-31', rentPrice: '$500', total: '$6000' },
-    { id: 2, policeNumber: 'B 5678 ABC', unitType: 'Sedan', contractEnd: '2025-06-15', rentPrice: '$400', total: '$4800' },
-    { id: 3, policeNumber: 'B 9101 DEF', unitType: 'Truck', contractEnd: '2024-08-01', rentPrice: '$700', total: '$8400' },
-    { id: 4, policeNumber: 'B 2468 GHI', unitType: 'MPV', contractEnd: '2025-03-20', rentPrice: '$450', total: '$5400' },
-    { id: 5, policeNumber: 'B 1357 JKL', unitType: 'Hatchback', contractEnd: '2024-10-10', rentPrice: '$350', total: '$4200' },
-    { id: 6, policeNumber: 'B 3690 MNO', unitType: 'SUV', contractEnd: '2025-01-05', rentPrice: '$550', total: '$6600' },
-    { id: 7, policeNumber: 'B 2580 PQR', unitType: 'Sedan', contractEnd: '2024-11-30', rentPrice: '$420', total: '$5040' },
-    { id: 8, policeNumber: 'B 1470 STU', unitType: 'Truck', contractEnd: '2025-04-15', rentPrice: '$750', total: '$9000' },
-    { id: 9, policeNumber: 'B 3692 VWX', unitType: 'MPV', contractEnd: '2024-09-25', rentPrice: '$480', total: '$5760' },
-    { id: 10, policeNumber: 'B 1593 YZA', unitType: 'Hatchback', contractEnd: '2025-02-28', rentPrice: '$380', total: '$4560' },
-  ];
+  const [companies, setCompanies] = useState<any[]>([]);
+  const [idCompany, setIdCompany] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        // Get id company
+        const userData = await AsyncStorage.getItem("userData");
+        if (userData) {
+          const getstorage = JSON.parse(userData);
+          setIdCompany(getstorage.id_company);
+        } else {
+          setIdCompany(null);
+        }
+      } catch (error) {
+        setError("Failed to fetch data");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (idCompany) {
+      const fetchData = async () => {
+        setIsLoading(true);
+        try {
+          const response = await fetch(`http://localhost:5182/api/v1/mobil-perusahaan/${idCompany}`);
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          const data = await response.json();
+          console.log("caca", data);
+          
+          setCompanies(data);
+        } catch (error: any) {
+          setError("Failed to fetch data: " + error.message);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+      fetchData();
+    }
+  }, [idCompany, location.key]);
+
+  function formatCurrency(value : any) {
+    const numberValue = typeof value === 'string' ? parseFloat(value.replace(/[^0-9.-]/g, '')) : value;
+  
+    if (isNaN(numberValue)) {
+      console.error('Value is not a valid number:', value);
+      return 'Rp -'; 
+    }
+    return 'Rp ' + numberValue.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+  }
+  
+  
 
   const columns = [
     {
@@ -28,34 +77,41 @@ const Vehicle = () => {
       sort: true
     },
     {
-      dataField: 'policeNumber',
+      dataField: 'nopol',
       text: 'Nomor Polisi',
       headerStyle: { backgroundColor: '#009879', color: 'white' },
       sort: true
     },
     {
-      dataField: 'unitType',
+      dataField: 'tb_perusahaan.nama_perusahaan',
+      text: 'Perusahaan',
+      headerStyle: { backgroundColor: '#009879', color: 'white' },
+      sort: true
+    },
+    {
+      dataField: 'tipe_kendaraan',
       text: 'Tipe Unit',
       headerStyle: { backgroundColor: '#009879', color: 'white' },
       sort: true
     },
     {
-      dataField: 'contractEnd',
+      dataField: 'jangka_waktu_sewa',
+      text: 'Mulai Kontrak',
+      headerStyle: { backgroundColor: '#009879', color: 'white' },
+      sort: true
+    },
+    {
+      dataField: 'contract_end',
       text: 'Akhir Kontrak',
       headerStyle: { backgroundColor: '#009879', color: 'white' },
       sort: true
     },
     {
-      dataField: 'rentPrice',
+      dataField: 'biaya_sewa',
       text: 'Harga Sewa',
       headerStyle: { backgroundColor: '#009879', color: 'white' },
-      sort: true
-    },
-    {
-      dataField: 'total',
-      text: 'Total',
-      headerStyle: { backgroundColor: '#009879', color: 'white' },
-      sort: true
+      sort: true,
+      formatter: (cell: any) => formatCurrency(cell),
     },
   ];
 
@@ -69,34 +125,40 @@ const Vehicle = () => {
   const { SearchBar } = Search;
 
   return (
-    <div className="container mt-5" style={{marginBottom:'10%'}}>
+    <div className="container mt-5" style={{ marginBottom: '10%' }}>
       <h1 className="text-center mb-4 text-dark font-weight-bold">Vehicle</h1>
 
-      <ToolkitProvider
-        keyField="id"
-        data={vehicles}
-        columns={columns}
-        search
-      >
-        {
-          props => (
-            <div>
-              <div className="mb-3">
-                <SearchBar {...props.searchProps} placeholder="Search" className="form-control" />
+      {isLoading ? (
+        <div className="text-center">Loading...</div>
+      ) : error ? (
+        <div className="text-center text-danger">{error}</div>
+      ) : (
+        <ToolkitProvider
+          keyField="id"
+          data={companies}
+          columns={columns}
+          search
+        >
+          {
+            props => (
+              <div>
+                <div className="mb-3">
+                  <SearchBar {...props.searchProps} placeholder="Search" className="form-control" />
+                </div>
+                <BootstrapTable
+                  {...props.baseProps}
+                  bootstrap4
+                  striped
+                  hover
+                  bordered={false}
+                  pagination={paginationFactory(paginationOptions)}
+                  classes="table-bordered w-100"
+                />
               </div>
-              <BootstrapTable
-                {...props.baseProps}
-                bootstrap4
-                striped
-                hover
-                bordered={false}
-                pagination={paginationFactory(paginationOptions)}
-                classes="table-bordered w-100"
-              />
-            </div>
-          )
-        }
-      </ToolkitProvider>
+            )
+          }
+        </ToolkitProvider>
+      )}
 
       <style>
         {`
