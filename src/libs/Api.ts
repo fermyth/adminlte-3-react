85396834f22b12f2b4728f93a2fedf6b64a2898d
alsyah07 <46066495+alsyah07@users.noreply.github.com>
2024-08-19 09:12:23 +0700

    Update Api.ts

--- src/libs/Api.ts
+++ src/libs/Api.ts
@@ -13,8 +13,8 @@ const ApiPartner = () => {
 };
 
 const UrlServer = () => {
-  // return "https://api_portal.sigapdriver.com";
-   return "https://api_partner_staging.sigapdriver.com/api/v1";
+   return "https://api_portal.sigapdriver.com";
+  // return "https://api_partner_staging.sigapdriver.com/api/v1";
  // return "http://localhost:5181";
 };
 
