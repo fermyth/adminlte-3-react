715a92b2503ecfef9738975933d7c045b0ba28eb
alsyah07 <46066495+alsyah07@users.noreply.github.com>
2024-11-03 15:49:16 +0700

    Update Api.ts

--- src/libs/Api.ts
+++ src/libs/Api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const ApiConfig = axios.create({
- baseURL: "http://localhost:5195/api/v1",
+ // baseURL: "http://localhost:5181/api/v1",
   // baseURL: "https://api_portal.sigapdriver.com/api/v1",
-  //  baseURL: "https://api_portal_staging.sigapdriver.com/api/v1",
+   baseURL: "https://api_portal_staging.sigapdriver.com/api/v1",
   // baseURL: "https://backend.sigapdriver.com/api/"
 });
 
@@ -13,9 +13,9 @@ const ApiPartner = () => {
 };
 
 const UrlServer = () => {
-  //  return "https://api_portal_staging.sigapdriver.com/api/v1";
+   return "https://api_portal_staging.sigapdriver.com/api/v1";
   // return "https://api_partner_staging.sigapdriver.com/api/v1";
-  return "http://localhost:5195/api/v1";
+ // return "http://localhost:5181/api/v1";
 };
 
 const UrlServerLaravel = () => {
